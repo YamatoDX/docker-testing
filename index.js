b67be8891af94ce5aa2287c96b6c88e818564fef
YamatoDX <piyal@twurs.com>
2022-02-25 02:04:41 +0600

    index.js file done

---
+++ index.js
@@ -0,0 +1,17 @@
+const app = require("express")();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    name: "shadman",
+    age: 20,
+    profession: "web developer",
+  });
+});
+
+app.listen(4000, () => {
+  console.log("listening to PORT 4000");
+});
